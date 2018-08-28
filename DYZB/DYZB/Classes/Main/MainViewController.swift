5cb44affd08c7cfbe26b652acedff2813cceb4d1
byron <byron@macbook-pro.local>
2018-08-28 19:06:59 +0800

    update mainstoryboard

---
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by byron on 2018/8/28.
+//  Copyright © 2018年 byron. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
