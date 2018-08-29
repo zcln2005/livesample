95c74af9d202b373912b710d07d5fbf6e53e83a0
byron <byron@macbook-pro.local>
2018-08-29 10:45:45 +0800

    设置UI界面

--- DYZB/DYZB/Classes/Main/MainViewController.swift
+++ DYZB/DYZB/Classes/Main/MainViewController.swift
@@ -12,8 +12,20 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        addChildVc("Home")
+        addChildVc("Live")
+        addChildVc("Follow")
+        addChildVc("Profile")
+    }
+    
+    
+    fileprivate func addChildVc(_ storyName : String) {
+        // 1.通过storyboard获取控制器
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        // 2.将childVc作为子控制器
+        addChildViewController(childVc)
     }
 
 
